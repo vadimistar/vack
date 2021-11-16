1d28ad8db11c0a4ed03a981f5e14ac07319a815d
Vadimistar <vadimistar@ya.ru>
2021-11-16 21:14:36 +0700

    Simplify value generation

--- src/Machine.cpp
+++ src/Machine.cpp
@@ -17,7 +17,20 @@ void Machine::executeInstr() {
     exit(1);
   };
 
+  const auto getStackTop = [this, &stackUnderflow] () {
+    const auto val { stackTop() };
+    if (!val) { stackUnderflow(); }
+    return val.value();
+  };
+
+  const auto getStackPop = [this, &stackUnderflow] () {
+    const auto val { stackTop() };
+    if (!stackPop()) { stackUnderflow(); }
+    return val.value();
+  };
+
   const auto instr = getInstr();
+
   switch (instr.kind) {
     case Instruction::Nop:
       break;
@@ -25,33 +38,16 @@ void Machine::executeInstr() {
       isHalt = true;
       break;
     case Instruction::Printi:
-      if (const auto val {stackTop()}; val) {
-        std::cout << std::bit_cast<std::int64_t>(val.value()) << '\n';  
-      } else {
-        stackUnderflow();
-      }
+      std::cout << std::bit_cast<std::int64_t>(getStackTop()) << '\n';  
       break;
     case Instruction::Printu:
-      if (const auto val {stackTop()}; val) {
-        std::cout << val.value() << '\n';  
-      } else {
-        stackUnderflow();
-      }
+      std::cout << getStackTop() << '\n';  
       break;
     case Instruction::Printf:
-      if (const auto val {stackTop()}; val) {
-        std::cout << std::bit_cast<double>(val.value()) << '\n';  
-      } else {
-        stackUnderflow();
-      }
+      std::cout << std::bit_cast<double>(getStackTop()) << '\n';  
       break;
     case Instruction::Printp:
-      if (const auto val{ stackTop() }; val) {
-        std::cout << std::bit_cast<void *>(val.value()) << '\n'; 
-      }
-      else {
-        stackUnderflow();
-      }
+      std::cout << std::bit_cast<void *>(getStackTop()) << '\n';  
       break;
     case Instruction::Push:
       if (!stackPush(instr.operand)) {
@@ -63,21 +59,11 @@ void Machine::executeInstr() {
         stackUnderflow();
       }
       break;
-    case Instruction::Add: {
-        const auto rhs { stackTop() };
-        if (!stackPop()) { stackUnderflow(); }
-        const auto lhs { stackTop() };
-        if (!stackPop()) { stackUnderflow(); }
-        if (!stackPush(lhs.value() + rhs.value())) { stackOverflow(); }
-      }
+    case Instruction::Add:
+      if (!stackPush(getStackPop() + getStackPop())) { stackOverflow(); }
       break;
-    case Instruction::Sub: {
-        const auto rhs { stackTop() };
-        if (!stackPop()) { stackUnderflow(); }
-        const auto lhs { stackTop() };
-        if (!stackPop()) { stackUnderflow(); }
-        if (!stackPush(lhs.value() - rhs.value())) { stackOverflow(); }
-      }
+    case Instruction::Sub: 
+      if (!stackPush(-(getStackPop()) + getStackPop())) { stackOverflow(); }
       break;
     default:
       assert(0 && "this kind of instruction is not handled");
